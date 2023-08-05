3a854d72e351397d964df6f36a1aa37aac9a9af9
NexTechGen <141403098+NexTechGen@users.noreply.github.com>
2023-08-05 06:01:18 +0000

    link_Bootstarp

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_jovin():
-  return render_template("home.html")
+  return render_template(
+    "home_bootstrap.html")  # home.html or home_bootstrap.html
 
 
 if __name__ == "__main__":
