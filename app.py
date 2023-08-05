9213aee2d19587ccb1d5da754dc2bbba7cac88e0
NexTechGen <141403098+NexTechGen@users.noreply.github.com>
2023-08-05 07:49:05 +0000

    Addded_dynamic_data

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
 # flask is model & Flask,render_templete is class
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Banguluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 'Rs. $120,000'
+  },
+]
 
 
 @app.route("/")
 def hello_jovin():
   return render_template(
-    "home_bootstrap.html")  # home.html or home_bootstrap.html
+    "home_bootstrap.html", jobs=JOBS,
+    company_name="Jovian")  # home.html or home_bootstrap.html
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
